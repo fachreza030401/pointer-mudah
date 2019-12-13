17cb4b545f1c15b5d8e6a2768ab5a9f9a32baa90
fachreza030401 <56225614+fachreza030401@users.noreply.github.com>
2019-12-13 20:53:31 +0700

    Add files via upload

---
+++ pointer mudah.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+                int a,*b;
+                b=&a;
+               cin >> a;
+                cout<<"Pointer b Menunjukkan : " <<b<<endl;
+                cout<<"Menunjukkan Nilai : " <<*b<<endl;
+
+                return 0;
+
+}
